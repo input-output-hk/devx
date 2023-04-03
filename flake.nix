27f0abc2faccd75ac4bb74cce1a6e99dfcea4088
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-04-04 01:20:44 +1200

    Build darwin shells on hydra

--- flake.nix
+++ flake.nix
@@ -73,7 +73,7 @@
        };
        supportedSystems = [
             "x86_64-linux"
-#            "x86_64-darwin"
+            "x86_64-darwin"
 #            "aarch64-linux"
 #            "aarch64-darwin"
        ];
