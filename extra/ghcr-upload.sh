0ce401d8e258a2d7c41f3ee7eb53109a1220792c
Andrea Bedini <andrea.bedini@tweag.io>
2023-04-06 15:19:27 +0800

    Use oras rather than custom scripts

--- extra/ghcr-upload.sh
+++ extra/ghcr-upload.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p zstd
+#! nix-shell -i bash -p zstd -p oras
 set -euo pipefail
 
 nix build ".#hydraJobs.${DEV_SHELL}"
-nix-store --export $(nix-store -qR result) | zstd -z8T8 > closure.zstd
-skopeo copy "dir:$(./extra/mk-docker-manifest.sh closure.zstd)" "docker://ghcr.io/input-output-hk/devx:${DEV_SHELL}"
+nix-store --export $(nix-store -qR result) | zstd -z8T8 >${DEV_SHELL}
+oras push ghcr.io/input-output-hk/devx:${DEV_SHELL} ${DEV_SHELL}
