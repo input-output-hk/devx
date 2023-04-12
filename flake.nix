278c7120d17e6836d6e876b1548d9ecede9bf901
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-04-13 01:04:29 +1200

    Use hkm/aarch64-musl branch of haskell.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
     description = "Minimal devshell flake for haskell";
 
-    inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+    inputs.haskellNix.url = "github:input-output-hk/haskell.nix/hkm/aarch64-musl";
     inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     inputs.flake-utils.url = "github:numtide/flake-utils";
 
