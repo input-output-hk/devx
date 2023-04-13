3b94ee582086ab3647455ba3673215f070b8e218
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-04-13 18:36:40 +1200

    Add --show-trace

--- extra/ghcr-upload.sh
+++ extra/ghcr-upload.sh
@@ -2,6 +2,6 @@
 #! nix-shell -i bash -p zstd -p oras
 set -euo pipefail
 
-nix build ".#hydraJobs.${DEV_SHELL}"
+nix build ".#hydraJobs.${DEV_SHELL}" --show-trace
 nix-store --export $(nix-store -qR result) | zstd -z8T8 >${DEV_SHELL}
 oras push ghcr.io/input-output-hk/devx:${DEV_SHELL} ${DEV_SHELL}
