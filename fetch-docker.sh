8ec3f5bedeff079e3c96086c298a7d2de3fb0185
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-04-14 02:19:45 +1200

    Avoid overhead of oras

--- fetch-docker.sh
+++ fetch-docker.sh
@@ -1,4 +1,18 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p zstd -p oras
+#!/bin/bash
 
-oras pull ghcr.io/input-output-hk/devx:$1
+TOKEN=$(curl --silent https://ghcr.io/token\?scope\=repository:$1:pull | jq -r .token)
+
+BLOB=$(curl \
+--silent \
+--request 'GET' \
+--header "Authorization: Bearer $TOKEN" \
+--header "Accept: application/vnd.docker.distribution.manifest.list.v2+json" \
+--header "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+--header "Accept: application/vnd.oci.image.manifest.v1+json" \
+"https://ghcr.io/v2/$1/manifests/$2" | tee manifest.json | jq -r '.layers[0].digest')
+
+curl \
+--location \
+--request GET \
+--header "Authorization: Bearer ${TOKEN}" \
+"https://ghcr.io/v2/$1/blobs/${BLOB}"
