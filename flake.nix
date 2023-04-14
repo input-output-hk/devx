ea91075950e541e88d6502ca2093e980cc0f0965
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-04-14 13:48:48 +1200

    Turn off darwin for now

--- flake.nix
+++ flake.nix
@@ -73,9 +73,9 @@
        };
        supportedSystems = [
             "x86_64-linux"
-            "x86_64-darwin"
+            # "x86_64-darwin"
             "aarch64-linux"
-            "aarch64-darwin"
+            # "aarch64-darwin"
        ];
     in flake-utils.lib.eachSystem supportedSystems (system:
          let
