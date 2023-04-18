caa9edc67f4f727b8d08a554bd09806c637ec60c
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-04-18 13:46:26 +1200

    Check if derivation path can be found

--- extra/ghcr-upload.sh
+++ extra/ghcr-upload.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p zstd -p oras
-set -euo pipefail
+set -euox pipefail
 
+nix path-info --derivation ".#hydraJobs.${DEV_SHELL}" --show-trace
 nix build ".#hydraJobs.${DEV_SHELL}" --show-trace
 nix-store --export $(nix-store -qR result) | zstd -z8T8 >${DEV_SHELL}
 oras push ghcr.io/input-output-hk/devx:${DEV_SHELL} ${DEV_SHELL}
