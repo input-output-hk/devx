6ec3480fcfe3e6e37f00bc7a5d9237168d600d15
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-04-18 16:30:23 +1200

    remove test step

--- extra/ghcr-upload.sh
+++ extra/ghcr-upload.sh
@@ -2,7 +2,6 @@
 #! nix-shell -i bash -p zstd -p oras
 set -euox pipefail
 
-nix path-info --derivation ".#hydraJobs.${DEV_SHELL}" --show-trace
 nix build ".#hydraJobs.${DEV_SHELL}" --show-trace
 nix-store --export $(nix-store -qR result) | zstd -z8T8 >${DEV_SHELL}
 oras push ghcr.io/input-output-hk/devx:${DEV_SHELL} ${DEV_SHELL}
