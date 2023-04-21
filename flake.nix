4acee2d44f5923f3b2f2085fd349864098854e7c
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-04-21 21:49:04 +1200

    Include aarch64-darwin

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
             "x86_64-linux"
             "x86_64-darwin"
             "aarch64-linux"
-            # "aarch64-darwin"
+            "aarch64-darwin"
        ];
     in flake-utils.lib.eachSystem supportedSystems (system:
          let
