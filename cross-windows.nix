af3e37394d8c0befa55320e512fd70cf11bf5f87
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-04-28 11:02:17 +1200

    Use pkgsBuildBuild for cddl and cbor-diag

--- cross-windows.nix
+++ cross-windows.nix
@@ -146,7 +146,7 @@ pkgs.pkgsBuildBuild.mkShell ({
     ++ pkgs.lib.optional (withHLS && (compiler-not-in (["ghc961"] ++ pkgs.lib.optional (pkgs.stdenv.hostPlatform.isDarwin && pkgs.stdenv.hostPlatform.isAarch64) "ghc902") "Haskell Language Server")) (tool "haskell-language-server")
     ++ pkgs.lib.optional (withHlint && (compiler-not-in (["ghc961"] ++ pkgs.lib.optional (pkgs.stdenv.hostPlatform.isDarwin && pkgs.stdenv.hostPlatform.isAarch64) "ghc902") "HLint")) (tool "hlint")
     ++ pkgs.lib.optional withIOG
-        (with pkgs; [ cddl cbor-diag ]
+        (with pkgs.pkgsBuildBuild; [ cddl cbor-diag ]
         ++ map pkgs.lib.getDev (with pkgs; [
             libblst libsodium-vrf secp256k1 #R_4_1_3
         ]))
