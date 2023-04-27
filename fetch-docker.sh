57f6fe2d2cefdcca0544af89a1efb9451a502d91
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-04-27 17:12:23 +1200

    Add comments

--- fetch-docker.sh
+++ fetch-docker.sh
@@ -2,6 +2,7 @@
 
 TOKEN=$(curl --silent https://ghcr.io/token\?scope\=repository:$1:pull | jq -r .token)
 
+# Read the manifest file from the docker image containing our nix-store closure and extract the layer url.
 BLOB=$(curl \
 --silent \
 --request 'GET' \
@@ -11,6 +12,7 @@ BLOB=$(curl \
 --header "Accept: application/vnd.oci.image.manifest.v1+json" \
 "https://ghcr.io/v2/$1/manifests/$2" | tee manifest.json | jq -r '.layers[0].digest')
 
+# Download the docker image layer that contains our nix store closure, and import it.
 curl \
 --location \
 --request GET \
