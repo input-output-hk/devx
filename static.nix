a392e73267575a80c5198dde1ad6caca6b142693
Moritz Angermann <moritz.angermann@gmail.com>
2023-05-03 19:55:10 +0800

    Update static.nix

--- static.nix
+++ static.nix
@@ -15,6 +15,7 @@ let tool-version-map = import ./tool-map.nix;
             case "$nixlib" in
             *libiconv.dylib) install_name_tool -change "$nixlib" /usr/lib/libiconv.dylib "$1" ;;
             *libffi.*.dylib) install_name_tool -change "$nixlib" /usr/lib/libffi.dylib   "$1" ;;
+            *libc++.*.dylib) install_name_tool -change "$nixlib" /usr/lib/libc++.dylib   "$1" ;;
             *libz.dylib)     install_name_tool -change "$nixlib" /usr/lib/libz.dylib     "$1" ;;
             *) ;;
             esac
