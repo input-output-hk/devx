2ad1c30834089961008753a452687210dc98f8c6
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-06-02 21:41:57 +1200

    Fix login

--- .github/workflows/release-ghcr-upload.sh
+++ .github/workflows/release-ghcr-upload.sh
@@ -5,5 +5,5 @@ set -euox pipefail
 if ! { [ "${GITHUB_REF:-}" == "refs/heads/master" ] || [[ "${GITHUB_REF:-}" = refs/heads/release* ]]; }; then
   exit 0;
 else
-  oras push ghcr.io/input-output-hk/cardano-node:${IMAGE_NAME} ${IMAGE_NAME}
+  oras push ghcr.io/input-output-hk/devx:${IMAGE_FILE} ${IMAGE_FILE}
 fi
