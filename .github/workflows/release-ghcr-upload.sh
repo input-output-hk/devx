c288b9dcc6bfe54efcbfde5a3712d9912662fef7
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-06-03 00:20:28 +1200

    Upload from test branch

--- .github/workflows/release-ghcr-upload.sh
+++ .github/workflows/release-ghcr-upload.sh
@@ -2,7 +2,7 @@
 #! nix-shell -i bash -p zstd -p oras
 set -euox pipefail
 
-if ! { [ "${GITHUB_REF:-}" == "refs/heads/master" ] || [[ "${GITHUB_REF:-}" = refs/heads/release* ]]; }; then
+if ! { [ "${GITHUB_REF:-}" == "refs/heads/hkm/cardano-node-ghcr-test" ] || [[ "${GITHUB_REF:-}" = refs/heads/release* ]]; }; then
   exit 0;
 else
   oras push ghcr.io/input-output-hk/devx:${IMAGE_FILE} ${IMAGE_FILE}
