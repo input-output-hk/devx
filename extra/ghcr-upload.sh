43d7fb43e58a8bc4e35e72f00786633e3387c4d8
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-06-09 14:18:32 +1200

    Avoid building in GHA
    GHA should be able to download the required files from the nix cache populated by hydra.  If it cannot DL what it needs, it is better to have it fail instead of wasting resources trying a build that will probably eventually run out of disk space anyway.

--- extra/ghcr-upload.sh
+++ extra/ghcr-upload.sh
@@ -2,6 +2,6 @@
 #! nix-shell -i bash -p zstd -p oras
 set -euox pipefail
 
-nix build ".#hydraJobs.${DEV_SHELL}" --show-trace
+nix build --builders "" --max-jobs 0 ".#hydraJobs.${DEV_SHELL}" --show-trace
 nix-store --export $(nix-store -qR result) | zstd -z8T8 >${DEV_SHELL}
 oras push ghcr.io/input-output-hk/devx:${DEV_SHELL} ${DEV_SHELL}
