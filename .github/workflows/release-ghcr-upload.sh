0b8904f6da47e9f9014ec632d5933fea70f812f9
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-06-15 18:39:28 +1200

    Try docker push

--- .github/workflows/release-ghcr-upload.sh
+++ .github/workflows/release-ghcr-upload.sh
@@ -1,9 +1,13 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p zstd -p oras
 set -euox pipefail
 
 if ! { [ "${GITHUB_REF:-}" == "refs/heads/hkm/cardano-node-ghcr-test" ] || [[ "${GITHUB_REF:-}" = refs/heads/release* ]]; }; then
   exit 0;
 else
-  oras push --artifact-type application/vnd.docker.image.v1+tar.gz ghcr.io/input-output-hk/devx:${IMAGE_FILE} ${IMAGE_FILE}
+  docker load < dockerImage
+  docker image tag inputoutput/cardano-node:$GITHUB_SHA ghcr.io/input-output-hk/cardano-node:$GITHUB_REF_NAME
+  docker push ghcr.io/input-output-hk/cardano-node:$GITHUB_REF_NAME
+  docker load < submitApiDockerImage
+  docker image tag inputoutput/cardano-submit-api:$GITHUB_SHA ghcr.io/input-output-hk/cardano-submit-api:$GITHUB_REF_NAME
+  docker push ghcr.io/input-output-hk/cardano-submit-api:$GITHUB_REF_NAME
 fi
