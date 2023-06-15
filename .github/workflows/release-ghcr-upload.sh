c417464876bf9b6d335fe268fae64c445fa4c8ae
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-06-15 20:14:49 +1200

    Try docker push

--- .github/workflows/release-ghcr-upload.sh
+++ .github/workflows/release-ghcr-upload.sh
@@ -2,8 +2,8 @@
 set -euox pipefail
 
 docker load < dockerImage
-docker image tag inputoutput/cardano-node:$GITHUB_SHA ghcr.io/input-output-hk/cardano-node:$GITHUB_REF_NAME
+docker image tag inputoutput/cardano-node:a7803c24de6a97d72cd025f71631ea65d078c19e ghcr.io/input-output-hk/cardano-node:$GITHUB_REF_NAME
 docker push ghcr.io/input-output-hk/cardano-node:$GITHUB_REF_NAME
 docker load < submitApiDockerImage
-docker image tag inputoutput/cardano-submit-api:$GITHUB_SHA ghcr.io/input-output-hk/cardano-submit-api:$GITHUB_REF_NAME
+docker image tag inputoutput/cardano-submit-api:a7803c24de6a97d72cd025f71631ea65d078c19e ghcr.io/input-output-hk/cardano-submit-api:$GITHUB_REF_NAME
 docker push ghcr.io/input-output-hk/cardano-submit-api:$GITHUB_REF_NAME
