c69f18582c9a2940161db3d5bc0e824c812ea850
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-06-15 19:59:02 +1200

    Try docker push

--- .github/workflows/release-ghcr-upload.sh
+++ .github/workflows/release-ghcr-upload.sh
@@ -1,13 +1,9 @@
 #! /usr/bin/env bash
 set -euox pipefail
 
-if ! { [ "${GITHUB_REF:-}" == "refs/heads/hkm/cardano-node-ghcr-test" ] || [[ "${GITHUB_REF:-}" = refs/heads/release* ]]; }; then
-  exit 0;
-else
-  docker load < dockerImage
-  docker image tag inputoutput/cardano-node:$GITHUB_SHA ghcr.io/input-output-hk/cardano-node:$GITHUB_REF_NAME
-  docker push ghcr.io/input-output-hk/cardano-node:$GITHUB_REF_NAME
-  docker load < submitApiDockerImage
-  docker image tag inputoutput/cardano-submit-api:$GITHUB_SHA ghcr.io/input-output-hk/cardano-submit-api:$GITHUB_REF_NAME
-  docker push ghcr.io/input-output-hk/cardano-submit-api:$GITHUB_REF_NAME
-fi
+docker load < dockerImage
+docker image tag inputoutput/cardano-node:$GITHUB_SHA ghcr.io/input-output-hk/cardano-node:$GITHUB_REF_NAME
+docker push ghcr.io/input-output-hk/cardano-node:$GITHUB_REF_NAME
+docker load < submitApiDockerImage
+docker image tag inputoutput/cardano-submit-api:$GITHUB_SHA ghcr.io/input-output-hk/cardano-submit-api:$GITHUB_REF_NAME
+docker push ghcr.io/input-output-hk/cardano-submit-api:$GITHUB_REF_NAME
