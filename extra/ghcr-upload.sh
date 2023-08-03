76dd191e5307a8888273ec92cd250b852f06c550
Moritz Angermann <moritz.angermann@gmail.com>
2023-08-03 20:21:57 +0200

    Update ghcr-upload.sh

--- extra/ghcr-upload.sh
+++ extra/ghcr-upload.sh
@@ -3,5 +3,15 @@
 set -euox pipefail
 
 nix build ".#hydraJobs.${DEV_SHELL}" --show-trace
-nix-store --export $(nix-store -qR result) | zstd -z8T8 >${DEV_SHELL}
-oras push ghcr.io/input-output-hk/devx:${DEV_SHELL} ${DEV_SHELL}
+echo <<EOF >${DEV_SHELL}
+#!/bin/bash
+BODY=\$(grep -n '# NIX_STORE' "\$0" | cut -d ':' -f 1)
+tail -n +\$((MARKER_LINE + 1)) "\$0" | nix-store --import
+exit 0
+cp $(readlink result) \$1
+# NIX_STORE
+EOF
+
+nix-store --export $(nix-store -qR result) >> ${DEV_SHELL}
+zstd -z8T8 ${DEV_SHELL} ${DEV_SHELL}.zst
+oras push ghcr.io/input-output-hk/devx:${DEV_SHELL} ${DEV_SHELL}.zst
