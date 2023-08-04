a6a3b79e030323728a64099d24503b7a8079c0b0
Yvan Sraka <yvan@sraka.xyz>
2023-08-04 13:41:54 +0200

    Fix `ghcr-upload.sh` by adding `--accept-flake-config` to `nix build` (#91)

--- extra/ghcr-upload.sh
+++ extra/ghcr-upload.sh
@@ -2,7 +2,7 @@
 #! nix-shell -i bash -p zstd -p oras
 set -euox pipefail
 
-nix build ".#hydraJobs.${DEV_SHELL}" --show-trace
+nix build ".#hydraJobs.${DEV_SHELL}" --show-trace --accept-flake-config
 nix-store --export $(nix-store -qR result) | zstd -z8T8 >${DEV_SHELL} | tee store-paths.txt
 if [[ ! $(tail -n 1 store-paths.txt) =~ "devx" ]]; then exit 1; fi
 oras push ghcr.io/input-output-hk/devx:${DEV_SHELL} ${DEV_SHELL}
