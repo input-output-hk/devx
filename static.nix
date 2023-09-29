0f931b87781f04de9ddcc7cad8d59c4b26235d86
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-09-29 13:13:55 +1300

    Fix eval

--- static.nix
+++ static.nix
@@ -135,7 +135,7 @@ pkgs.mkShell (rec {
         stdenv.cc.cc.lib ]) ++ (with pkgs.buildPackages; [
     ])
     ++ pkgs.lib.optional (withHLS && (compiler-not-in (["ghc980" "ghc99"] ++ pkgs.lib.optional (pkgs.stdenv.hostPlatform.isDarwin && pkgs.stdenv.hostPlatform.isAarch64) "ghc902") "Haskell Language Server")) (tool "haskell-language-server")
-    ++ pkgs.lib.optional (withHlint && (compiler-not-in (["ghc961" "ghc962" "ghc980" "ghc99"] ++ pkgs.lib.optional (pkgs.stdenv.hostPlatform.isDarwin && pkgs.stdenv.hostPlatform.isAarch64) "ghc902") "HLint")) (tool "hlint")
+    ++ pkgs.lib.optional (withHlint && (compiler-not-in (["ghc961" "ghc962" "ghc980" "ghc99"] ++ pkgs.lib.optional (pkgs.stdenv.hostPlatform.isAarch64) "ghc902") "HLint")) (tool "hlint")
     ++ pkgs.lib.optional withIOG (with pkgs; [ cddl cbor-diag ])
     ;
 })
