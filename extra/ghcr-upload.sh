8524da456b15c950bffc21d9be5cebccad33a244
Moritz Angermann <moritz.angermann@gmail.com>
2025-02-01 20:13:06 +0900

    Update ghcr-upload.sh (#180)
    Drop using nix build, which has a tendency to rebuild stuff. And use nix path-info and nix-store directly.

--- extra/ghcr-upload.sh
+++ extra/ghcr-upload.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p zstd -p oras
+#! nix-shell -i bash -p zstd -p oras -p jq
 set -euox pipefail
 
-nix build ".#hydraJobs.${DEV_SHELL}" --show-trace --accept-flake-config
-nix-store --export $(nix-store -qR result) | tee store-paths.txt | zstd -z8T8 >${DEV_SHELL}
+SHELL_NIX_PATH=$(nix path-info ".#hydraJobs.${DEV_SHELL}" --accept-flake-config --json | jq -r 'keys[0]')
+nix-store -r "$SHELL_NIX_PATH"
+#nix build ".#hydraJobs.${DEV_SHELL}" --show-trace --accept-flake-config
+nix-store --export $(nix-store -qR "$SHELL_NIX_PATH") | tee store-paths.txt | zstd -z8T8 >${DEV_SHELL}
 if [[ ! $(tail -n 1 store-paths.txt) =~ "devx" ]]; then exit 1; fi
 oras push ghcr.io/input-output-hk/devx:${DEV_SHELL} ${DEV_SHELL}
