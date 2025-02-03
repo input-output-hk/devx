f1e9152c19f587d65066c30352a20b71f0863ee1
Moritz Angermann <moritz.angermann@gmail.com>
2025-02-03 09:42:29 +0900

    Update ghcr-upload.sh

--- extra/ghcr-upload.sh
+++ extra/ghcr-upload.sh
@@ -6,7 +6,7 @@ set -euox pipefail
 : "${GITHUB_SHA:?'GITHUB_SHA is not set'}"
 : "${DEV_SHELL:?'DEV_SHELL is not set'}"
 
-SHELL_NIX_PATH=$(nix path-info "github:${GITHUB_REPOSITORY}/${GITHUB_SHA}#hydraJobs.${DEV_SHELL}" --accept-flake-config --no-update-lock-file --json | jq -r 'keys[0]')
+SHELL_NIX_PATH=$(nix path-info "github:${GITHUB_REPOSITORY}/${GITHUB_SHA}#hydraJobs.${DEV_SHELL}" --accept-flake-config --json | jq -r 'keys[0]')
 nix-store -r "$SHELL_NIX_PATH"
 #nix build ".#hydraJobs.${DEV_SHELL}" --show-trace --accept-flake-config
 nix-store --export $(nix-store -qR "$SHELL_NIX_PATH") | tee store-paths.txt | zstd -z8T8 >${DEV_SHELL}
