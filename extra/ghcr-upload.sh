09b36dc6cce6e32875201c204ff51f4db31e18fa
Moritz Angermann <moritz.angermann@gmail.com>
2025-02-06 13:29:28 +0900

    retry is the solution :facepalm:

--- extra/ghcr-upload.sh
+++ extra/ghcr-upload.sh
@@ -5,7 +5,8 @@ set -euox pipefail
 : "${DEV_SHELL:?'DEV_SHELL is not set'}"
 : "${SHELL_NIX_PATH:?'SHELL_NIX_PATH is not set'}"
 
-nix-store -r "$SHELL_NIX_PATH"
+# retry three times in case of intermittent network errors.
+nix-store -r "$SHELL_NIX_PATH" || nix-store -r "$SHELL_NIX_PATH" || nix-store -r "$SHELL_NIX_PATH"
 #nix build ".#hydraJobs.${DEV_SHELL}" --show-trace --accept-flake-config
 nix-store --export $(nix-store -qR "$SHELL_NIX_PATH") | tee store-paths.txt | zstd -z8T8 >${DEV_SHELL}
 if [[ ! $(tail -n 1 store-paths.txt) =~ "devx" ]]; then exit 1; fi
